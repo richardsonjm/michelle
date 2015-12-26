e61cdf7b5f7a4a61bc911d79abae01faca61f1a9
John Richardson <richardsonjm@gmail.com>
2015-12-26 18:40:01 -0500

    Allow for editing of retained image

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -59,6 +59,6 @@ class WorksController < ApplicationController
     end
 
     def work_params
-      params.require(:work).permit(:client, :campaign, :challange, :insight, :outcome, :results, :logo, works_images_attributes: [:image])
+      params.require(:work).permit(:client, :campaign, :challange, :insight, :outcome, :results, :logo, works_images_attributes: [:image, :_destroy, :id])
     end
 end
